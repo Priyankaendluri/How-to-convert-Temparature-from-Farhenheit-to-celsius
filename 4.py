57418035d7173a6782273237fd4696f20ecb292c
Priyankaendluri <endlurupriyanka@gmail.com>
2025-02-24 22:34:57 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,6 @@
+def Farhenheit_celsius (f):
+   celsius=5/9*(f-32)
+   return celsius
+T=int(input("Enter Temparatue in to Farhenheit"))
+Farhenheit_Temparature=Farhenheit_celsius (T)
+print(Farhenheit_Temparature)
